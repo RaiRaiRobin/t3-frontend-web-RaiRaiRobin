fe12ac4123ec98023b58dfa1077611abcd292baa
RaiRaiRobin <rai.ribin1000@gmail.com>
2019-06-22 21:44:06 +0545

    login design updated

--- resources/js/userCustom.js
+++ resources/js/userCustom.js
@@ -166,8 +166,11 @@ $(document).ready(function() {
                 window.location.href = "../user/userDashboard.html";
             },
             error: function(jqXHR, status) {
-                //  console.log(jqXHR.responseJSON.message);
-                $('#message').html(jqXHR.responseJSON.message);
+                 // console.log(jqXHR.responseJSON.message);
+                // $('#message').html(jqXHR.responseJSON.message);
+                alert(jqXHR.responseJSON.message);
+                $('#LoginPassword').val('');
+                $('#LoginPassword').focus();
             }
         });
 
